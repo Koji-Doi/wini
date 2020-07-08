1a76902df780c90954db2f22e300a3826bb1917e
Koji Doi <doikoji@gmail.com>
2020-07-09 03:32:36 +0900

    modified:   wini.pm

--- wini.pm
+++ wini.pm
@@ -117,7 +117,7 @@ use Encode;
 $Data::Dumper::Useperl = 1 ;
 
 my $scriptname = basename($0);
-my $version    = "ver. 0 rel. 20200519";
+my $version    = "ver. 0 rel. 20200709";
 my @save;
 my %ref; # $ref{image}{imageID} = 1; keys of %$ref: qw/image table formula citation math ref/
 my $debug;
