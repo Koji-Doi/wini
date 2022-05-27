ddf5dc5c3e79a8a5197ef54ae7d00dfdd1fe18cb
Koji Doi <doikoji@gmail.com>
2022-05-27 23:00:45 +0900

    modified:   utils/mk_t_from_sh.pl

--- utils/mk_t_from_sh.pl
+++ utils/mk_t_from_sh.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use utf8;
 use Data::Dumper;
+use File::Temp 'tempdir';
+
 
 no warnings;
 *Data::Dumper::qquote = sub { return encode "utf8", shift } ;
@@ -19,9 +21,25 @@ binmode STDERR, ':utf8';
 # save STDERR to hoge.t.stderr
 # save new test script to hoge.t
 
+my $tempdir1 = tempdir('tXXXX') or die 'Failed to create temp dir';
+chdir $tempdir1;
+my $fho;
+while(<DATA>){
+  chomp;
+  if(/^---start\s+(\S+)/){
+    print STDERR "Trying to create $1\n";
+    open($fho, '>:utf8', $1) or die "Failed to open temp file '$1'";
+  }elsif(/^---end/){
+    close $fho;
+  }else{
+    (defined fileno $fho) and print {$fho} "$_\n";
+  }
+}
+
+# do_it('touch a.txt', 'a_out.txt', 'a_err.txt');
 sub do_it{
   my($cmd, $cmd_outfile, $cmd_errfile) = @_;
-#  my($cmd_infile, $cmd_errfile, $cmd_outfile) = ('','','');
+#  my($cmd_infile);
 #  $cmd=~s{\s*<\s*(\S+)}{ $cmd_infile=$1;  ''}eg;
 #  $cmd=~s{\s*2>\s*(\S+)}{$cmd_errfile=$1; ''}eg;
 #  $cmd=~s{\s*>\s*(\S+)}{ $cmd_outfile=$1; ''}eg;
@@ -39,5 +57,14 @@ sub do_it{
 }
 
 my($cmd, $outfile) = @_;
+($cmd)   or $cmd    ='touch a.txt';
 $outfile or $outfile='out';
 do_it($cmd, "$outfile.out", "$outfile.err");
+
+__DATA__
+---start a.txt
+This is a.txt.
+---start b.txt
+This is b.txt.
+---end
+
